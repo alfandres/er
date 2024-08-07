74d0ac4e1d5784b6eea02af74c0b046b30e602b4
Andres Parra <alfandres2108@gmail.com>
2024-08-07 17:38:01 -0500

    x2correccion e implementacion de assets en forma de .js

--- src/assets/svgs.js
+++ src/assets/svgs.js
@@ -1,8 +1,8 @@
 import LogoCall from "./svg/logo-Call.svg";
 import LogoEmail from "./svg/logo-Email.svg";
 import LogoEr from "./svg/logo-Er.svg";
-import LogoErBlack from "./svg/logo-Er-black.svg";
 import LogoErWhite from "./svg/logo-Er-white.svg";
+
 import LogoFacebook from "./svg/logo-Facebook.svg";
 import logoInstagram from "./svg/logo-Instagram.svg";
 import logoWhatsapp from "./svg/logo-Whatsapp.svg";
@@ -11,7 +11,6 @@ const svgs = {
   LogoCall: LogoCall,
   LogoEmail: LogoEmail,
   LogoEr: LogoEr,
-  LogoErBlack: LogoErBlack,
   LogoErWhite: LogoErWhite,
   LogoFacebook: LogoFacebook,
   logoInstagram: logoInstagram,
